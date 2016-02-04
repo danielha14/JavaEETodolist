e0ee72d4fc4e56f68c7e03cbc4d4a15cefd55468
danielha1@gmail.com <danielha!@gmail.com>
2016-02-04 11:23:03 +0200

    adding Query that return Id by email

--- finalProjectToDoList/src/com/project/todolist/model/HibernateToDoListDAO.java
+++ finalProjectToDoList/src/com/project/todolist/model/HibernateToDoListDAO.java
@@ -1,7 +1,10 @@
 package com.project.todolist.model;
 
+
+import java.util.Arrays;
 import java.util.List;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
@@ -9,7 +12,7 @@ import org.hibernate.cfg.AnnotationConfiguration;
 /**
  * The Class HibernateToDoListDAO.
  */
-public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
+public class HibernateToDoListDAO implements IToDoListDAO , IToDoListUser{
 
 	/** The factory. */
 	private SessionFactory factory = null;
@@ -20,7 +23,8 @@ public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
 	/**
 	 * Constructor HibernateToDoListDAO.
 	 */
-	private HibernateToDoListDAO() {
+	private HibernateToDoListDAO(){
+
 
 		factory = new AnnotationConfiguration().configure().buildSessionFactory();
 
@@ -38,111 +42,115 @@ public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
 		return instance;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.abelski.samples.hibernate.IToDoListUser#addUser(com.abelski.samples.
-	 * hibernate.User)
+	/* (non-Javadoc)
+	 * @see com.abelski.samples.hibernate.IToDoListUser#addUser(com.abelski.samples.hibernate.User)
 	 */
 	@Override
 	public void addUser(User user) throws TodoListPlatformException {
 		Session session = factory.openSession();
-		try {
+		try{
 
 			session.beginTransaction();
-			// if(!(session.get(user.getClass(),user.getEmail()) != null))
-			// {
+			//if(!(session.get(user.getClass(),user.getEmail()) != null))
+			//{
 			session.save(user);
 			session.getTransaction().commit();
 
-			// }
-			// else
-			// .out.println("user "+user.getUserName() +" is exists");
+			//	}
+			//else 
+			//.out.println("user "+user.getUserName() +"  is exists");
 
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with addUser method");
 			}
 
-		} finally {
-			try {
-				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+		}
+		finally{
+			try{session.close();}
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with addItem method");
 				}
 
-			}
-		}
+			}}
 	}
 
 	@Override
 	public User getUserByEmail(String email) throws TodoListPlatformException {
 		Session session = factory.openSession();
 		User user = null;
-		try {
-			user = (User) session.get(User.class, 1);
+		try{
+
 			session.beginTransaction();
-			if ((user != null)) {
-
+			//Query for select Id(return only Id) by email
+			Query query = session.createQuery("select id from User as u where u.email= :user_email");
+			query.setParameter("user_email", email);
+	        List<Integer> userIdList = (List<Integer>) query.list();
+	        //check if exist such id
+			if(userIdList.size()>0)
+			{
 				session.getTransaction().commit();
-				// user =(User) session.get(User.class, email);
+				user = (User) session.get(User.class, userIdList.get(0));
+				
 			}
-			// else
-			// System.out.println("user "+user.getUserName() +" is exists");
-
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+		 
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with getUserById method");
 			}
 
-		} finally {
-			try {
+		}
+		finally{
+			try
+			{
 				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+			}
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with getUserById  method");
 				}
 
-			}
-		}
+			}}
 		return user;
 
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.abelski.samples.hibernate.IToDoListUser#deleteUser(com.abelski.
-	 * samples.hibernate.User)
+
+	/* (non-Javadoc)
+	 * @see com.abelski.samples.hibernate.IToDoListUser#deleteUser(com.abelski.samples.hibernate.User)
 	 */
 	@Override
 	public boolean deleteUser(User user) throws TodoListPlatformException {
 		Session session = factory.openSession();
-		try {
+		try{
 			session.beginTransaction();
 			session.delete(user);
 			session.getTransaction().commit();
 
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with deteteUser method");
 			}
 
-		} finally {
-			try {
-				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+		}
+		finally{
+			try{ session.close();}
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with deleteUser method");
 				}
@@ -151,105 +159,96 @@ public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
 		}
 		return true;
 
+
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.abelski.samples.hibernate.IToDoListUser#updateUser(com.abelski.
-	 * samples.hibernate.User)
+	/* (non-Javadoc)
+	 * @see com.abelski.samples.hibernate.IToDoListUser#updateUser(com.abelski.samples.hibernate.User)
 	 */
 	@Override
-	public void updateUser(User userToUpdate) throws TodoListPlatformException {
+	public void updateUser(User userToUpdate) throws TodoListPlatformException{
 		Session session = factory.openSession();
-		try {
+		try{
 
 			session.beginTransaction();
 			session.update(userToUpdate);
 			session.getTransaction().commit();
 
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with updateUser method");
 			}
 
-		} finally {
-			try {
-				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+		}
+		finally{
+			try{session.close();}
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with updateUser method");
 				}
 
-			}
-		}
+			}}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.abelski.samples.hibernate.IToDoListDAO#addItem(com.abelski.samples.
-	 * hibernate.ToDoItem)
+	/* (non-Javadoc)
+	 * @see com.abelski.samples.hibernate.IToDoListDAO#addItem(com.abelski.samples.hibernate.ToDoItem)
 	 */
 	@Override
 	public void addItem(ToDoItem itemFromUserToAdd) throws TodoListPlatformException {
 		Session session = factory.openSession();
-		try {
+		try{
 
 			session.beginTransaction();
 			session.save(itemFromUserToAdd);
 			session.getTransaction().commit();
 
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with addItem method");
 			}
 
-		} finally {
-			try {
-				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+		}
+		finally{
+			try{session.close();}
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with addItem method");
 				}
 
-			}
-		}
+			}}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.abelski.samples.hibernate.IToDoListDAO#deleteItem(com.abelski.samples
-	 * .hibernate.ToDoItem)
+	/* (non-Javadoc)
+	 * @see com.abelski.samples.hibernate.IToDoListDAO#deleteItem(com.abelski.samples.hibernate.ToDoItem)
 	 */
 	@Override
 	public boolean deleteItem(ToDoItem itemFromUserToDelete) throws TodoListPlatformException {
 		Session session = factory.openSession();
-		try {
+		try{
 			session.beginTransaction();
 			session.delete(itemFromUserToDelete);
 			session.getTransaction().commit();
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with deleteItem method");
 			}
 
-		} finally {
-			try {
-				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+		}
+		finally{
+			try{ session.close();}
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with deleteItem method");
 				}
@@ -258,45 +257,40 @@ public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
 		}
 		return true;
 
+
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.abelski.samples.hibernate.IToDoListDAO#updateItem(com.abelski.samples
-	 * .hibernate.ToDoItem)
+	/* (non-Javadoc)
+	 * @see com.abelski.samples.hibernate.IToDoListDAO#updateItem(com.abelski.samples.hibernate.ToDoItem)
 	 */
 	public void updateItem(ToDoItem itemFromUserToUpdate) throws TodoListPlatformException {
 		Session session = factory.openSession();
-		try {
+		try{
 			session.beginTransaction();
 			session.update(itemFromUserToUpdate);
 			session.getTransaction().commit();
 
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with updateItem method");
 			}
 
-		} finally {
-			try {
-				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+		}
+		finally{
+			try{session.close();}
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with updateItem method");
 				}
 
-			}
-		}
+			}}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
+	/* (non-Javadoc)
 	 * @see com.abelski.samples.hibernate.IToDoListDAO#getItems()
 	 */
 	@SuppressWarnings("finally")
@@ -304,22 +298,23 @@ public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
 	public List getItems() throws TodoListPlatformException {
 		Session session = factory.openSession();
 		List items = null;
-		try {
+		try{
 			session.beginTransaction();
 			items = session.createQuery("from ToDoItem").list();
 
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with getItems method");
 			}
 
-		} finally {
-			try {
-				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+		}
+		finally{
+			try{ session.close();  }
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with getItems method");
 				}
@@ -327,6 +322,7 @@ public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
 			}
 			return items;
 		}
+
 
 	}
 
@@ -334,22 +330,23 @@ public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
 	public List<ToDoItem> getUserItems(int id) throws TodoListPlatformException {
 		Session session = factory.openSession();
 		List items = null;
-		try {
+		try{
 			session.beginTransaction();
-			items = session.createQuery("from ToDoItem where USERID=" + id).list();
+			items = session.createQuery("from ToDoItem where USERID="+id).list();
 
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with getItems method");
 			}
 
-		} finally {
-			try {
-				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+		}
+		finally{
+			try{ session.close();  }
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with getItems method");
 				}
@@ -359,12 +356,13 @@ public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
 		}
 	}
 
+
 	public boolean authenticateUser(String userEmail, String password) throws TodoListPlatformException {
-		User user = getUserByEmail(userEmail);
-		if (user != null && user.getEmail().equals(userEmail) && user.getPassword().equals(password)) {
+		User user = getUserByEmail(userEmail);         
+		if(user!=null && user.getEmail().equals(userEmail) && user.getPassword().equals(password)){
 
 			return true;
-		} else {
+		}else{
 			return false;
 		}
 	}
@@ -373,37 +371,47 @@ public class HibernateToDoListDAO implements IToDoListDAO, IToDoListUser {
 	public ToDoItem getItemById(int itemId) throws TodoListPlatformException {
 		Session session = factory.openSession();
 		ToDoItem item = null;
-		try {
+		try{
 
 			session.beginTransaction();
-			if ((session.get(ToDoItem.class, itemId) != null)) {
+			if((session.get(ToDoItem.class,itemId)!= null))
+			{
 
 				session.getTransaction().commit();
-				item = (ToDoItem) session.get(ToDoItem.class, itemId);
+				item =(ToDoItem) session.get(ToDoItem.class, itemId);
 			}
-			// else
-			// System.out.println("user "+user.getUserName() +" is exists");
+			//else 
+			//	System.out.println("user "+user.getUserName() +"  is exists");
 
-		} catch (HibernateException e) {
-			if (session.getTransaction() != null) {
+
+		}
+		catch(HibernateException e){
+			if(session.getTransaction()!=null){
 
 				session.getTransaction().rollback();
 				throw new TodoListPlatformException("error with getUserById method");
 			}
 
-		} finally {
-			try {
+		}
+		finally{
+			try
+			{
 				session.close();
-			} catch (HibernateException e) {
-				if (session.getTransaction() != null) {
+			}
+			catch(HibernateException e){
+				if(session.getTransaction()!=null){
 					session.getTransaction().rollback();
 					throw new TodoListPlatformException("error with getUserById  method");
 				}
 
-			}
-		}
+			}}
 		return item;
 
 	}
 
+
+
+
+
 }
+
